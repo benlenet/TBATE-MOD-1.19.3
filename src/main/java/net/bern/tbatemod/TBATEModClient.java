8c36654e612240122809721203b15d107e174c32
Le <benlenet@csu.fullerton.edu>
2023-02-06 11:41:05 -0800

    Initial commit

---
+++ src/main/java/net/bern/tbatemod/TBATEModClient.java
@@ -0,0 +1,10 @@
+package net.bern.tbatemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TBATEModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
