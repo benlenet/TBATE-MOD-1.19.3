7aa2560a9b643e1e8a82429bb021fb9387e285ce
Ben Le <benlenet@csu.fullerton.edu>
2023-02-07 19:54:37 -0800

    added png

--- src/main/java/net/bern/tbatemod/item/ModItems.java
+++ src/main/java/net/bern/tbatemod/item/ModItems.java
@@ -11,16 +11,20 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    // list of items: name cannot be capital
     public static final Item SYLVIE_ROCK = registerItem("sylvie_rock", new Item(new FabricItemSettings()));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(TBATEMod.MOD_ID, name), item);
     }
     public static void addItemsToItemGroup() {
         addToItemGroup(ItemGroups.INGREDIENTS, SYLVIE_ROCK);
     }
+
     private static void addToItemGroup(ItemGroup group, Item item) {
         ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
     }
+    // initializes class so that all items are properly registered
     public static void registerModItems() {
         TBATEMod.LOGGER.info("Registering Mod Items for " + TBATEMod.MOD_ID);
     }
